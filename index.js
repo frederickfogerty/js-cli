10a6ad988b9b74d2562cb10ae59507d70febcab5
Frederick Fogerty <frederick.fogerty@gmail.com>
2016-11-23 12:46:52 +1300

    Use dist again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 process.env.TS_NODE_FAST = true;
 
-require("ts-babel-node/register");
-require("babel-polyfill");
+// require("ts-babel-node/register");
+// require("babel-polyfill");
 
-require("./src/index.ts");
+require("./dist");
