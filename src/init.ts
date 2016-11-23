b7d336d4adcf3bfd0aa366a7c9f8b0e5aaeab05a
Frederick Fogerty <frederick.fogerty@gmail.com>
2016-11-24 08:05:06 +1300

    Add version flag

--- src/init.ts
+++ src/init.ts
@@ -1,3 +1,6 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { loadavg } from 'os';
 import { log } from './common/util/log';
 
 
@@ -9,8 +12,20 @@ process.on('unhandledRejection', function (reason: Error, promise: Promise<any>)
 import * as common from './common';
 import cli from 'command-interface';
 import config from './common/config';
+import * as minimist from 'minimist';
+
+function checkVersionFlag() {
+	const argv: { version?: boolean, v?: boolean } = minimist(process.argv.slice(1));
+	return argv.version || argv.v;
+}
 
 async function init(cb: Function) {
+	if (checkVersionFlag()) {
+		log.info.blue(require(path.resolve(__dirname, '../package.json')).version);
+		return;
+	}
+
+
 	cli(config.SCRIPTS_DIRS);
 	cb();
 }
