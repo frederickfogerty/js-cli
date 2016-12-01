0a3a5ff7f8a597deff086266df25f86393cc251b
Frederick Fogerty <frederick.fogerty@gmail.com>
2016-12-01 18:54:13 +1300

    Allow prompt to return multiple packages

--- src/common/prompt.ts
+++ src/common/prompt.ts
@@ -2,23 +2,26 @@ import { R } from './util';
 import * as inquirer from 'inquirer';
 import * as constants from './constants';
 
+const ALL_MODULES_LABEL = 'all modules'
 
 /**
  * Prompts the user to select a module.
  */
 export async function forModule(
-  pkgs: constants.IPackageObject[],
-  message = 'Module'
-): Promise<constants.IPackageObject> {
+	pkgs: constants.IPackageObject[],
+	message = 'Module',
+	opts: { allowAll?: boolean } = { allowAll: true },
+): Promise<constants.IPackageObject[]> {
+	const choices = (opts.allowAll ? [ALL_MODULES_LABEL] : []).concat(pkgs.map(pkg => pkg.name))
 
-  const confirm = {
-    type: 'list',
-    name: 'name',
-    message,
-    choices: pkgs.map(pkg => pkg.name),
-  };
-  const { name } = (await inquirer.prompt(confirm));
-  return R.find(R.propEq('name', name), pkgs);
+	const confirm = {
+		type: 'list',
+		name: 'name',
+		message,
+		choices,
+	};
+	const { name } = (await inquirer.prompt(confirm));
+	return name === ALL_MODULES_LABEL ? pkgs : [R.find(R.propEq('name', name), pkgs)];
 }
 
 
@@ -27,12 +30,12 @@ export async function forModule(
  * Prompts the user whether they sure they want to continue.
  */
 export async function confirm(message: string = 'Are you sure?'): Promise<boolean> {
-  const confirm = {
-    type: 'confirm',
-    name: 'result',
-    message,
-  };
-  const { result } = (await inquirer.prompt(confirm)) as any;
-  return result;
+	const confirm = {
+		type: 'confirm',
+		name: 'result',
+		message,
+	};
+	const { result } = (await inquirer.prompt(confirm)) as any;
+	return result;
 }
 
