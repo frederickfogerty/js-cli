eacde22f26576d995b9a128d69f7c3d882615974
Frederick Fogerty <frederick.fogerty@gmail.com>
2016-12-01 18:53:50 +1300

    Add execaCommand function

--- src/common/util/execa.ts
+++ src/common/util/execa.ts
@@ -9,10 +9,24 @@ const split = require('split');
 export const execa = (cmd: string, args?: string[], opts?: ExecFileOptions) => {
 	// Use `Observable` support if merged https://github.com/sindresorhus/execa/pull/26
 	const cp = Execa(cmd, args, opts);
-	// cp.then((a) => console.log('a', cmd, args, a));
 
 	return Observable.merge(
 		streamToObservable(cp.stdout.pipe(split()), { await: cp }),
 		streamToObservable(cp.stderr.pipe(split()), { await: cp })
 	).filter(Boolean);
 };
+
+/**
+ * Executes a command by splitting it into the relevant file and arg sections
+ */
+export const execaCommand = (cmd: string, opts?: ExecFileOptions) => {
+	// const commands = cmd.split(' ');
+	// const file = commands[0];
+	// const args = commands.slice(1);
+	const cp = Execa.shell(cmd, opts);
+
+	return Observable.merge(
+		streamToObservable(cp.stdout.pipe(split()), { await: cp }),
+		streamToObservable(cp.stderr.pipe(split()), { await: cp })
+	).filter(Boolean);
+}
