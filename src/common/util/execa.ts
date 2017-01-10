24d6e8ce2e6350ec49cc949ed4a15368e1018d51
Frederick Fogerty <frederick.fogerty@gmail.com>
2017-01-11 11:04:43 +1300

    Add options to execa to return as a promise

--- src/common/util/execa.ts
+++ src/common/util/execa.ts
@@ -1,9 +1,10 @@
 import { execAsync } from './';
 import { ExecFileOptions } from 'child_process';
 import * as Execa from 'execa';
-const streamToObservable = require('stream-to-observable');
-const Observable = require('any-observable');
-const split = require('split');
+const streamToObservable = require('stream-to-observable'); // tslint:disable-line
+const Observable = require('any-observable'); // tslint:disable-line
+const split = require('split'); // tslint:disable-line
+const observableToPromise = require('observable-to-promise'); // tslint:disable-line
 
 
 export const execa = (cmd: string, args?: string[], opts?: ExecFileOptions) => {
@@ -12,21 +13,27 @@ export const execa = (cmd: string, args?: string[], opts?: ExecFileOptions) => {
 
 	return Observable.merge(
 		streamToObservable(cp.stdout.pipe(split()), { await: cp }),
-		streamToObservable(cp.stderr.pipe(split()), { await: cp })
+		streamToObservable(cp.stderr.pipe(split()), { await: cp }),
 	).filter(Boolean);
 };
 
 /**
  * Executes a command by splitting it into the relevant file and arg sections
  */
-export const execaCommand = (cmd: string, opts?: ExecFileOptions) => {
+export const execaCommand = (cmd: string, opts: ExecFileOptions & { promise?: boolean } = {}) => {
 	// const commands = cmd.split(' ');
 	// const file = commands[0];
 	// const args = commands.slice(1);
 	const cp = Execa.shell(cmd, opts);
 
-	return Observable.merge(
+	const data$ = Observable.merge(
 		streamToObservable(cp.stdout.pipe(split()), { await: cp }),
-		streamToObservable(cp.stderr.pipe(split()), { await: cp })
+		streamToObservable(cp.stderr.pipe(split()), { await: cp }),
 	).filter(Boolean);
-}
+
+	if (opts.promise) {
+		return observableToPromise(data$);
+	} else {
+		return data$;
+	}
+};
