46e2ea9e7ff00289f89284bec87af9fec78da29f
Frederick Fogerty <frederick.fogerty@gmail.com>
2017-02-02 09:02:45 +1300

    Add getModuleFromParams helper

---
+++ src/common/params.ts
@@ -0,0 +1,21 @@
+import { toPackagesArray } from './constants';
+import { pathToPackageObject } from './package';
+import * as constants from './constants';
+
+
+export function getModulesFromParams(
+	params: string[] = [],
+	modules: string[] = constants.MODULE_DIRS,
+): constants.IPackagesArray {
+
+	const isKeywordMatch = (pkg: constants.IPackageObject) => {
+		if (params.length === 0) { return true; }
+		return params.find((pattern) => pkg.name.indexOf(pattern) !== -1);
+	};
+
+	const filteredModules = modules
+		.filter((module) => isKeywordMatch(pathToPackageObject(module)));
+
+	return toPackagesArray(filteredModules);
+
+}
